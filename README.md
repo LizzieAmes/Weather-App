a34883f9d35fdd5710c13c6050b8b0a0ca004ce3
Lizzie <lizziejacoboo@gmail.com>
2023-11-27 11:46:47 -0700

    Fixed functions displaying weather icons, updated styles and added styles for different sizes, added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-App
+Welcome to my weather forecast website! I created this page as a way to check the current weather for any city, as well as get the next 5 days forecast. The weather forecast information comes from https://openweathermap.org/forecast5 and using this api, the webpage pulls the data and displays it for the user to view. There is also a function that saves the 10 most recent searched cities, and when clicked, it will display that city's weather forecast.
+
+![Weather App Screenshot](assets/images/App%20Screenshot.png)
+
+## References
+I had some help from an amazing youtube video on the GreatStack youtube channel. https://www.youtube.com/watch?v=MIYQR-Ybrn4&t=2061s. This video helped me get a better understanding of how to use the weather api and create functions to display the data pulled from the api. I used the video to help build the checkWeather function and used the weather icons the video provided. 
+
--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,20 @@ var apiKey = "5345d6a4a1422ad41c7a77b23fe70eab";
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial";
 var searchBox = document.querySelector(".search input");
 var searchBtn = document.querySelector(".search button");
-var weatherIcon = document.querySelector(".weather-icon");
 var recentSearchContainer = document.querySelector(".recent-searches");
 var recentSearches = JSON.parse(localStorage.getItem("recentSearches")) || [];
-var currentWeatherIcon = document.querySelector(".current-weather .weather-icon");
 
-
+// Saves recent searches
 function saveRecentSearch(city) {
-recentSearches.unshift(city);
-localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-updateRecentSearches();
+  recentSearches.unshift(city);
+  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  updateRecentSearches();
 }
 
 function updateRecentSearches() {
   recentSearchContainer.innerHTML = "";
-  // Loop through the first 8 items or all items if less than 8
-  for (let i = 0; i < Math.min(recentSearches.length, 8); i++) {
+  // Loop through the first 10 items or all items if less than 10
+  for (let i = 0; i < Math.min(recentSearches.length, 10); i++) {
     let button = document.createElement("button");
     button.textContent = recentSearches[i];
     button.addEventListener("click", () => checkWeather(recentSearches[i]));
@@ -26,99 +24,68 @@ function updateRecentSearches() {
 }
 
 async function checkWeather(city) {
-  const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+  var response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
   var data = await response.json();
 
-  console.log(data);
 
   document.querySelector(".city").innerHTML = data.city.name;
-
-  const date = new Date(data.list[0].dt_txt);
-  const formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }); 
+  
+  var date = new Date(data.list[0].dt_txt);
+  var formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
   document.querySelector(".date").innerHTML = formattedDate;
 
-  document.querySelector(".temp").innerHTML = "Temp: " + Math.round(data.list[0].main.temp) + "°F"
+  document.querySelector(".temp").innerHTML = "Temp: " + Math.round(data.list[0].main.temp) + "°F";
+  document.querySelector(".wind").innerHTML = "Wind: " + data.list[0].wind.speed + " MPH";
+  document.querySelector(".humidity").innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
 
-  document.querySelector(".wind").innerHTML = "Wind: " + data.list[0].wind.speed + " MPH"
+  // Display weather icon based on weather condition
+  if (data.list[0].weather[0].main == "Rain") {
+    document.querySelector("#current-weather-icon").src = "assets/images/rain.png";
+  } else if (data.list[0].weather[0].main == "Clouds") {
+    document.querySelector("#current-weather-icon").src = "assets/images/clouds.png";
+  } else if (data.list[0].weather[0].main == "Clear") {
+    document.querySelector("#current-weather-icon").src = "assets/images/clear.png";
+  } else if (data.list[0].weather[0].main == "Mist") {
+    document.querySelector("#current-weather-icon").src = "assets/images/mist.png";
+  } else if (data.list[0].weather[0].main == "Drizzle") {
+    document.querySelector("#current-weather-icon").src = "assets/images/drizzle.png";
+  } else if (data.list[0].weather[0].main == "Snow") {
+    document.querySelector("#current-weather-icon").src = "assets/images/snow.png";
+  }
 
-  document.querySelector(".humidity").innerHTML = "Humidity: " + data.list[0].main.humidity + "%"
+  // Display the next 5 days of forecast
+  for (let i = 0; i < 5; i++) {
+    var date = new Date(data.list[i * 8].dt_txt); 
+    var formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
-  
+    // Display forecast information for each day
+    document.querySelector(`.date-${i}`).innerHTML = formattedDate;
+    document.querySelector(`.temp-${i}`).innerHTML = "Temp: " + Math.round(data.list[i * 8].main.temp) + "°F";
+    document.querySelector(`.wind-${i}`).innerHTML = "Wind: " + data.list[i * 8].wind.speed + " MPH";
+    document.querySelector(`.humidity-${i}`).innerHTML = "Humidity: " + data.list[i * 8].main.humidity + "%";
 
-  setWeatherIcon(data.list[0].weather[0].main, "weather-icon");
-  const next5DaysContainer = document.querySelector(".next-5-days .futuredays");
-  next5DaysContainer.innerHTML = ""; // Clear previous content
-
-  for (let i = 1; i <= 5; i++) {
-    const dayData = data.list[i * 8 - 1]; 
-    const iconId = `weather-icon-${i}`;
-    console.log(`Generated ID: ${iconId}`);
-    
-    const dayDate = new Date(dayData.dt_txt);
-    const dayFormattedDate = dayDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
-    const dayTemp = "Temperature: " + Math.round(dayData.main.temp) + "°F";
-    const dayWind = "Wind: " + dayData.wind.speed + " MPH";
-    const dayHumidity = "Humidity: " + dayData.main.humidity + "%";
-
-    // Create a new div to represent each day
-    let dayDiv = document.createElement("div");
-    dayDiv.classList.add("day");
-
-    let iconElement = document.createElement("img");
-    iconElement.classList.add("weather-icon");
-    iconElement.id = `weather-icon-${i}`;
-    // Update the content for each day
-    dayDiv.innerHTML = `
-      <h4>${dayFormattedDate}</h4>
-      
-      <p>${dayTemp}</p>
-      <p>${dayWind}</p>
-      <p>${dayHumidity}</p>
-    `;
-    
-    setWeatherIcon(data.list[0].weather[0].main, iconId);
-    // Append the day div to the container
-    next5DaysContainer.appendChild(dayDiv);
+    // Display weather icon based on weather condition
+    if (data.list[i * 8].weather[0].main == "Rain") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/rain.png";
+    } else if (data.list[i * 8].weather[0].main == "Clouds") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/clouds.png";
+    } else if (data.list[i * 8].weather[0].main == "Clear") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/clear.png";
+    } else if (data.list[i * 8].weather[0].main == "Mist") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/mist.png";
+    } else if (data.list[i * 8].weather[0].main == "Drizzle") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/drizzle.png";
+    } else if (data.list[i * 8].weather[0].main == "Snow") {
+      document.querySelector(`.weather-icon-${i}`).src = "assets/images/snow.png";
+    }
   }
 
   saveRecentSearch(city);
-
 }
 
-function setWeatherIcon(weatherMain, elementId) {
-  // Set the weather icon based on the weather condition
-  const iconElement = document.getElementById(elementId);
-
-  if (iconElement) {
-    switch (weatherMain) {
-      case "Rain":
-        iconElement.src = "assets/images/rain.png";
-        break;
-      case "Clouds":
-        iconElement.src = "assets/images/clouds.png";
-        break;
-      case "Clear":
-        iconElement.src = "assets/images/clear.png";
-        break;
-      case "Mist":
-        iconElement.src = "assets/images/mist.png";
-        break;
-      case "Drizzle":
-        iconElement.src = "assets/images/drizzle.png";
-        break;
-      case "Snow":
-        iconElement.src = "assets/images/snow.png";
-        break;
-    }
-  } else {
-    console.error(`Element with ID '${elementId}' not found.`);
-  }
-}
-
-searchBtn.addEventListener("click", async ()=>{
+searchBtn.addEventListener("click", async () => {
   await checkWeather(searchBox.value);
   searchBox.value = "";
 });
 
 updateRecentSearches();
-
